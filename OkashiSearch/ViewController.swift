0790a26180088ba735995927bb584ec7c4ab2c46
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-11-16 15:30:37 +0900

    Initial Commit

---
+++ OkashiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OkashiSearch
+//
+//  Created by 曽和寛貴 on 2018/11/16.
+//  Copyright © 2018 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
